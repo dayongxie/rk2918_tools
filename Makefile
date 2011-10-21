1489dfb8a181a1826a439e9e5d728437ea4ccf62
Zhiyong Xie <dayong1111@gmail.com>
2011-10-21 17:32:29 +0800

    实现AFPTool

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
+all: afptool
 afptool : afptool.o
 
--- afptool.c
+++ afptool.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -8,48 +9,42 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/mman.h>
-#include <limits.h>
 
 #include "rkcrc.h"
 #include "rkafp.h"
 
 #define UPDATE_MAGIC	"RKAF"
 
-int
-check_update(const unsigned char *data, size_t data_len)
-{
-	int stored_crc, crc;
-	
+int check_update(const unsigned char *data, size_t data_len) {
+	unsigned int stored_crc = 0, crc = 0;
+
 	printf("Check file... ");
 	fflush(stdout);
-	
+
 	if (data_len < 4)
 		goto check_failed;
-	
+
 	memcpy(&stored_crc, &data[data_len - 4], 4);
 
-	RKCRC(crc, data, data_len);
-	
+	RKCRC(crc, data, data_len - 4);
+
 	if (crc != stored_crc)
 		goto check_failed;
 
 	printf("OK\n");
 
 	return 0;
-	
-check_failed:
-	printf("Failed\n");
+
+	check_failed: printf("Failed\n");
 	return -1;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // unpack functions
 
-int
-create_dir(char *dir)
-{
+int create_dir(char *dir) {
 	char *sep = dir;
-	while ((sep=strchr(sep,'/')) != NULL) {
+	while ((sep = strchr(sep, '/')) != NULL) {
 		*sep = '\0';
 		if (mkdir(dir, 0755) != 0 && errno != EEXIST) {
 			printf("Can't create directory: %s\n", dir);
@@ -63,61 +58,54 @@ create_dir(char *dir)
 	return 0;
 }
 
-int
-extract_file(void *data, size_t len, const char *path)
-{
+int extract_file(void *data, size_t len, const char *path) {
 	FILE *ofp;
 
-	if ((ofp=fopen(path, "wb")) == NULL) {
+	if ((ofp = fopen(path, "wb")) == NULL) {
 		printf("Can't open/create file: %s\n", path);
 		return -1;
 	}
 
 	fwrite(data, len, 1, ofp);
 	fclose(ofp);
-	
+
 	return 0;
 }
 
-int
-unpack_update(const char* srcfile, const char* dstdir)
-{
+int unpack_update(const char* srcfile, const char* dstdir) {
 	int fd = -1;
 	size_t file_len = 0;
 	struct update_header header;
 	void *buf = MAP_FAILED;
-	
+
 	fd = open(srcfile, O_RDONLY);
-	if ( fd == -1 )
-	{
-		fprintf(stderr, "can't open file \"%s\": %s\n", 
-			srcfile, strerror(errno));
+	if (fd == -1) {
+		fprintf(stderr, "can't open file \"%s\": %s\n", srcfile,
+				strerror(errno));
 		goto unpack_fail;
 	}
-	
+
 	file_len = lseek(fd, 0, SEEK_END);
-	
-	if (file_len == (size_t)-1)
+
+	if (file_len == (size_t) -1)
 		goto unpack_fail;
 
-	if (file_len < sizeof(header))
-	{
+	if (file_len < sizeof(header)) {
 		fprintf(stderr, "Invalid file size\n");
 		goto unpack_fail;
 	}
-	
+
 	buf = mmap(NULL, file_len, PROT_READ, MAP_SHARED | MAP_FILE, fd, 0);
-	
+
 	if (buf == MAP_FAILED)
 	{
 		perror("Error mmap");
 		goto unpack_fail;
 	}
-	
+
 	memcpy(&header, buf, sizeof(header));
-	
-	if (strncmp(header.magic, RKAFP_MAGIC, sizeof(header.magic)) != 0)
-	{
+
+	if (strncmp(header.magic, RKAFP_MAGIC, sizeof(header.magic)) != 0) {
 		fprintf(stderr, "Invalid header magic\n");
 		goto unpack_fail;
 	}
@@ -129,29 +117,29 @@ unpack_update(const char* srcfile, const char* dstdir)
 	if (header.num_parts) {
 		unsigned i;
 		char dir[PATH_MAX];
-		
-		for (i=0; i < header.num_parts; i++) {
+
+		for (i = 0; i < header.num_parts; i++) {
 			struct update_part *part = &header.parts[i];
-			printf("%s\t0x%08X\t0x%08X\n", part->filename, part->pos, part->size);
+			printf("%s\t0x%08X\t0x%08X\n", part->filename, part->pos,
+					part->size);
 
 			if (strcmp(part->filename, "SELF") == 0) {
 				printf("Skip SELF file.\n");
 				continue;
 			}
-			
+
 			// parameter 多出文件头8个字节,文件尾4个字节
 			if (memcmp(part->name, "parameter", 9) == 0) {
 				part->pos += 8;
 				part->size -= 12;
 			}
-			
+
 			snprintf(dir, sizeof(dir), "%s/%s", dstdir, part->filename);
-			
+
 			if (-1 == create_dir(dir))
 				continue;
 
-			if ( part->pos + part->size > file_len - 4 )
-			{
+			if (part->pos + part->size > file_len - 4) {
 				fprintf(stderr, "Invalid part: %s\n", part->name);
 				continue;
 			}
@@ -159,53 +147,47 @@ unpack_update(const char* srcfile, const char* dstdir)
 			extract_file(buf + part->pos, part->size, dir);
 		}
 	}
-	
+
 	munmap(buf, file_len);
 	close(fd);
 
 	return 0;
-	
-unpack_fail:
-	if (fd)
-	{
+
+	unpack_fail: if (fd) {
 		if (buf)
 			munmap(buf, file_len);
-			
+
 		close(fd);
 	}
-	
+
 	return -1;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // pack functions
 
-struct pack_part
-{
+struct pack_part {
 	char name[32];
 	char filename[32];
 	unsigned int nand_addr;
-	unsigned int padded_size;
-	unsigned int size;
+	unsigned int nand_size;
 };
 
-struct partition
-{
+struct partition {
 	char name[32];
 	unsigned int start;
 	unsigned int size;
 };
 
-typedef struct
-{
+typedef struct {
 	unsigned int version;
-	
-    char machine_model[0x22];
-    char machine_id[0x1e];
-    char manufacturer[0x38];
-	
+
+	char machine_model[0x22];
+	char machine_id[0x1e];
+	char manufacturer[0x38];
+
 	unsigned int num_package;
-	struct update_part packages[16];
+	struct pack_part packages[16];
 
 	unsigned int num_partition;
 	struct partition partitions[16];
@@ -213,80 +195,97 @@ typedef struct
 
 static PackImage package_image;
 
-int
-parse_partitions(char *str)
-{
-	char *nand, *parts, *token1;
-	char *part, *token2;
+int parse_partitions(char *str) {
+	char *nand, *parts;
+	char *part, *token1, *ptr;
 	struct partition *p_part;
-	
-	nand = strtok_r(str, ":", &token1);
-	parts = strtok_r(NULL, ":", &token1);
+	int i;
 
-	if (parts)
-	{
-		part = strtok_r(parts, ",", &token2);
+	nand = str;
+	parts = strchr(str, ':');
 
-		for (; part; part = strtok_r(NULL, ",", &token2) )
-		{
+	if (parts) {
+		*parts = '\0';
+		parts++;
+		part = strtok_r(parts, ",", &token1);
+
+		for (; part; part = strtok_r(NULL, ",", &token1)) {
 			p_part = &(package_image.partitions[package_image.num_partition]);
-			if (3 != sscanf(part, "0x%08x@0x%08x(%s)", &p_part->size, &p_part->start, p_part->name))
+
+			p_part->size = strtol(part, &ptr, 16);
+			ptr = strchr(ptr, '@');
+			if (!ptr)
+				continue;
+
+			ptr++;
+			p_part->start = strtol(ptr, &ptr, 16);
+
+			for (; *ptr && *ptr != '('; ptr++);
+
+			for (i = 0, ptr++; i < sizeof(p_part->name) && *ptr && *ptr != ')'; i++, ptr++)
 			{
-				if (2 !=  sscanf(part, "-@0x%08x(%s)", &p_part->start, p_part->name))
-					continue;
-				p_part->size = 0;
+				p_part->name[i] = *ptr;
 			}
+
+			if (i < sizeof(p_part->name))
+				p_part->name[i] = '\0';
+			else
+				p_part->name[i-1] = '\0';
 
 			package_image.num_partition++;
 		}
+
+		for (i = 0; i < package_image.num_partition; ++i)
+		{
+			p_part = &(package_image.partitions[i]);
+		}
 	}
-	
+
 	return 0;
 }
 
-int 
-action_parse_key(char *key, char *value)
-{
-	if ( strcmp(key, "FIRMWARE_VER") == 0 )
-	{
-		int a, b, c;
+int action_parse_key(char *key, char *value) {
+	if (strcmp(key, "FIRMWARE_VER") == 0) {
+		unsigned int a, b, c;
 		sscanf(value, "%d.%d.%d", &a, &b, &c);
-		package_image.version = a << 24 + b << 16 + c;
-	}
-	else if ( strcmp(key, "MACHINE_MODEL") == 0 )
-	{
-		package_image.machine_model[sizeof(package_image.machine_model) - 1] = 0;
-		strncpy(package_image.machine_model, value, sizeof(package_image.machine_model));
+		package_image.version = (a << 24) + (b << 16) + c;
+	} else if (strcmp(key, "MACHINE_MODEL") == 0) {
+		package_image.machine_model[sizeof(package_image.machine_model) - 1] =
+				0;
+		strncpy(package_image.machine_model, value,
+				sizeof(package_image.machine_model));
 		if (package_image.machine_model[sizeof(package_image.machine_model) - 1])
 			return -1;
-	}
-	else if ( strcmp(key, "MACHINE_ID") ==  0 )
-	{
+	} else if (strcmp(key, "MACHINE_ID") == 0) {
 		package_image.machine_id[sizeof(package_image.machine_id) - 1] = 0;
-		strncpy(package_image.machine_id,  value, sizeof(package_image.machine_id));
+		strncpy(package_image.machine_id, value,
+				sizeof(package_image.machine_id));
 		if (package_image.machine_id[sizeof(package_image.machine_id) - 1])
 			return -1;
-	}
-	else if ( strcmp(key, "MANUFACTURER") == 0 )
-	{
+	} else if (strcmp(key, "MANUFACTURER") == 0) {
 		package_image.manufacturer[sizeof(package_image.manufacturer) - 1] = 0;
-		strncpy(package_image.manufacturer, value, sizeof(package_image.manufacturer));
+		strncpy(package_image.manufacturer, value,
+				sizeof(package_image.manufacturer));
 		if (package_image.manufacturer[sizeof(package_image.manufacturer) - 1])
 			return -1;
-	}
-	else if ( strcmp(key, "CMDLINE") ==  0)
-	{
+	} else if (strcmp(key, "CMDLINE") == 0) {
 		char *param, *token1;
-		char *param_key, *param_value, *token2;
-		param =  strtok_r(value, " ", &token1);
-		
-		while (param)
-		{
-			param_key = strtok_r(param, "=", &token2);
-			param_value = strtok_r(NULL, "=", &token2);
+		char *param_key, *param_value;
+		param = strtok_r(value, " ", &token1);
 
-			if (strcmp(param_key, "mtdparts") == 0)
+		while (param) {
+			param_key = param;
+			param_value = strchr(param, '=');
+
+			if (param_value)
 			{
+				*param_value = '\0';
+				param_value++;
+			} else {
+				continue;
+			}
+
+			if (strcmp(param_key, "mtdparts") == 0) {
 				parse_partitions(param_value);
 			}
 			param = strtok_r(NULL, " ", &token1);
@@ -295,16 +294,12 @@ action_parse_key(char *key, char *value)
 	return 0;
 }
 
-int
-parse_parameter(const char *fname)
-{
-	char params[50][200];
-	char line[200], *startp, *endp;
-	char *key, *value, *tokenptr;
-	int param_count = 0;
+int parse_parameter(const char *fname) {
+	char line[512], *startp, *endp;
+	char *key, *value;
 	FILE *fp;
 
-	if ((fp=fopen(fname,"r")) == NULL) {
+	if ((fp = fopen(fname, "r")) == NULL) {
 		printf("Can't open file: %s\n", fname);
 		return -1;
 	}
@@ -312,25 +307,28 @@ parse_parameter(const char *fname)
 	while (fgets(line, sizeof(line), fp) != NULL) {
 		startp = line;
 		endp = line + strlen(line) - 1;
-		if (*endp != '\n' && *endp != '\r')
+		if (*endp != '\n' && *endp != '\r' && !feof(fp))
 			break;
-		
+
 		// trim line
 		while (isspace(*startp))
 			++startp;
-			
+
 		while (isspace(*endp))
 			--endp;
-		*endp = 0;
-		
+		endp[1] = 0;
+
 		if (*startp == '#' || *startp == 0)
 			continue;
 
-		key = strtok_r(startp, ":", &tokenptr);
-		value = strtok_r(NULL, ":", &tokenptr);
-		
-		if (!key || !value)
-			break;
+		key = startp;
+		value = strchr(startp, ':');
+
+		if (!value)
+			continue;
+
+		*value = '\0';
+		value++;
 
 		action_parse_key(key, value);
 	}
@@ -341,51 +339,312 @@ parse_parameter(const char *fname)
 		return -3;
 	}
 
+	fclose(fp);
+
 	return 0;
 }
 
-int
-pack_update(const char* srcdir, const char* dstfile)
+static struct partition first_partition =
 {
-	char buf[256];
+		"parameter",
+		0,
+		0x2000
+};
+
+struct partition* find_partition_byname(const char *name)
+{
+	int i;
+	struct partition *p_part;
+
+	for (i = package_image.num_partition - 1; i >= 0; i--)
+	{
+		p_part = &package_image.partitions[i];
+		if (strcmp(p_part->name, name) == 0)
+			return p_part;
+	}
+
+	if (strcmp(name, first_partition.name) == 0)
+	{
+		return &first_partition;
+	}
+
+	return NULL;
+}
+
+struct pack_part* find_package_byname(const char *name)
+{
+	int i;
+	struct pack_part *p_pack;
+
+	for (i = package_image.num_partition - 1; i >= 0; i--)
+	{
+		p_pack = &package_image.packages[i];
+		if (strcmp(p_pack->name, name) == 0)
+			return p_pack;
+	}
+
+	return NULL;
+}
+
+void append_package(const char *name, const char *path)
+{
+	struct partition *p_part;
+	struct pack_part *p_pack = &package_image.packages[package_image.num_package];
+
+	strncpy(p_pack->name, name, sizeof(p_pack->name));
+	strncpy(p_pack->filename, path, sizeof(p_pack->filename));
+
+	p_part = find_partition_byname(name);
+	if (p_part)
+	{
+		p_pack->nand_addr = p_part->start;
+		p_pack->nand_size = p_part->size;
+	} else {
+		p_pack->nand_addr = (unsigned int)-1;
+		p_pack->nand_size = 0;
+	}
+
+	package_image.num_package++;
+}
+
+int get_packages(const char *fname)
+{
+	char line[512], *startp, *endp;
+	char *name, *path;
+	FILE *fp;
+
+	if ((fp = fopen(fname, "r")) == NULL) {
+		printf("Can't open file: %s\n", fname);
+		return -1;
+	}
+
+	while (fgets(line, sizeof(line), fp) != NULL) {
+		startp = line;
+		endp = line + strlen(line) - 1;
+		if (*endp != '\n' && *endp != '\r' && !feof(fp))
+			break;
+
+		// trim line
+		while (isspace(*startp))
+			++startp;
+
+		while (isspace(*endp))
+			--endp;
+		endp[1] = 0;
+
+		if (*startp == '#' || *startp == 0)
+			continue;
+
+		name = startp;
+
+		while (*startp && *startp != ' ' && *startp != '\t')
+			startp++;
+
+		while (*startp == ' ' || *startp == '\t')
+		{
+			*startp = '\0';
+			startp++;
+		}
+
+		path = startp;
+
+		append_package(name, path);
+	}
+
+	if (!feof(fp)) {
+		printf("File read failed!\n");
+		fclose(fp);
+		return -3;
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+int import_package(FILE *ofp, struct update_part *pack, const char *path)
+{
+	FILE *ifp;
+	char buf[2048];
+	size_t readlen;
+
+	pack->pos = ftell(ofp);
+	ifp = fopen(path, "r");
+	if (!ifp)
+		return -1;
+
+	if (strcmp(pack->name, "parameter") == 0)
+	{
+		unsigned int crc;
+		struct param_header *header = (struct param_header*)buf;
+		strcpy(header->magic, "PARM");
+
+		readlen = fread(buf + sizeof(*header), 1, sizeof(buf) - 12, ifp);
+		header->length = readlen;
+		RKCRC(crc, buf + sizeof(*header), readlen);
+		readlen += sizeof(*header);
+		memcpy(buf + readlen, &crc, sizeof(crc));
+		readlen += sizeof(crc);
+		memset(buf+readlen, 0, sizeof(buf) - readlen);
+
+		fwrite(buf, 1, sizeof(buf), ofp);
+		pack->size += readlen;
+		pack->padded_size += sizeof(buf);
+	} else {
+		do {
+			readlen = fread(buf, 1, sizeof(buf), ifp);
+			if (readlen == 0)
+				break;
+
+			if (readlen < sizeof(buf))
+				memset(buf + readlen, 0, sizeof(buf) - readlen);
+
+			fwrite(buf, 1, sizeof(buf), ofp);
+			pack->size += readlen;
+			pack->padded_size += sizeof(buf);
+		} while (!feof(ifp));
+	}
+
+	fclose(ifp);
+
+	return 0;
+}
+
+void append_crc(const char* filename)
+{
+	int fd = -1;
+	unsigned int crc = 0;
+	size_t file_len = 0;
+	void *buf = MAP_FAILED;
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1) {
+		fprintf(stderr, "can't open file \"%s\": %s\n", filename,
+				strerror(errno));
+		goto fail;
+	}
+
+	file_len = lseek(fd, 0, SEEK_END);
+
+	if (file_len == (size_t) -1)
+		goto fail;
+
+	buf = mmap(NULL, file_len, PROT_READ, MAP_SHARED | MAP_FILE, fd, 0);
+
+	if (buf == MAP_FAILED)
+	{
+		perror("Error mmap");
+		goto fail;
+	}
+
+	printf("Add CRC...\n");
+
+	RKCRC(crc, buf, file_len);
+
+	munmap(buf, file_len);
+
+	lseek(fd, 0, SEEK_END);
+	write(fd, &crc, sizeof(crc));
+	close(fd);
+
+fail:
+	if (fd != -1)
+	{
+		if (buf != MAP_FAILED)
+			munmap(buf, file_len);
+
+		close(fd);
+	}
+}
+
+int pack_update(const char* srcdir, const char* dstfile) {
+	struct update_header header;
+	FILE *fp;
+	int i;
+	char buf[PATH_MAX];
 
 	printf("------ PACKAGE ------\n");
+	memset(&header, 0, sizeof(header));
 
 	snprintf(buf, sizeof(buf), "%s/%s", srcdir, "parameter");
 	if (parse_parameter(buf))
 		return -1;
 
+	snprintf(buf, sizeof(buf), "%s/%s", srcdir, "package-file");
+	if (get_packages(buf))
+		return -1;
+
+	fp = fopen(dstfile, "w");
+	if (!fp)
+		return -1;
+
+	fwrite(&header, sizeof(header), 1, fp);
+
+	for (i = 0; i < package_image.num_package; ++i) {
+		strcpy(header.parts[i].name, package_image.packages[i].name);
+		strcpy(header.parts[i].filename, package_image.packages[i].filename);
+		header.parts[i].nand_addr = package_image.packages[i].nand_addr;
+		header.parts[i].nand_size = package_image.packages[i].nand_size;
+
+		if (strcmp(package_image.packages[i].filename, "SELF") == 0)
+			continue;
+
+		snprintf(buf, sizeof(buf), "%s/%s", srcdir, header.parts[i].filename);
+		printf("Add file: %s\n", buf);
+		import_package(fp, &header.parts[i], buf);
+	}
+
+	strcpy(header.magic, "RKAF");
+	strcpy(header.manufacturer, package_image.manufacturer);
+	strcpy(header.model, package_image.machine_model);
+	strcpy(header.id, package_image.machine_id);
+	header.length = ftell(fp);
+	header.num_parts = package_image.num_package;
+	header.version = package_image.version;
+
+	for (i = header.num_parts - 1; i >= 0; --i)
+	{
+		if (strcmp(header.parts[i].filename, "SELF") == 0)
+		{
+			header.parts[i].size = header.length + 4;
+			header.parts[i].padded_size = (header.parts[i].size + 511) / 512 *512;
+		}
+	}
+
+	fseek(fp, 0, SEEK_SET);
+	fwrite(&header, sizeof(header), 1, fp);
+	fclose(fp);
+
+	append_crc(dstfile);
+
+	printf("------ OK ------\n");
+
 	return 0;
 }
 
-void
-usage(const char *appname)
-{
-	const char *p = strrchr(appname,'/');
+void usage(const char *appname) {
+	const char *p = strrchr(appname, '/');
 	p = p ? p + 1 : appname;
 
-	printf(	"USAGE:\n"
-		"\t%s {Option} Src [Dest]\n"
-		"Example:\n"
-		"\t%s -pack xxx update.img\tPack files\n"
-		"\t%s -unpack update.img xxx\tunpack files\n",
-		p, p, p, p);
+	printf("USAGE:\n"
+			"\t%s <-pack|-unpack> <Src> <Dest>\n"
+			"Example:\n"
+			"\t%s -pack xxx update.img\tPack files\n"
+			"\t%s -unpack update.img xxx\tunpack files\n", p, p, p, p);
 }
 
-int
-main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	if (argc < 3) {
 		usage(argv[0]);
 		return 1;
 	}
 
-	if (strcmp(argv[1], "-pack") == 0 && argc >= 3) {
+	if (strcmp(argv[1], "-pack") == 0 && argc == 4) {
 		if (pack_update(argv[2], argv[3]) == 0)
 			printf("Pack OK!\n");
 		else
 			printf("Pack failed\n");
-	} else if (strcmp(argv[1], "-unpack") == 0 && argc >= 3) {
+	} else if (strcmp(argv[1], "-unpack") == 0 && argc == 4) {
 		if (unpack_update(argv[2], argv[3]) == 0)
 			printf("UnPack OK!\n");
 		else
